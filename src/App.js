18505542395f04886ff8f45c6913447ce619db77
Longanhne <longnh11092000@gmail.com>
2024-05-02 16:57:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { darkTheme } from '../src/Theme/DarkTheme';
+import { Navbar } from './component/Navbar/Navbar';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ThemeProvider theme={darkTheme}>
+     <CssBaseline/>
+     <Navbar />
+     </ThemeProvider>
     </div>
   );
 }
-
 export default App;
